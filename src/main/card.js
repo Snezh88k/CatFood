113839d29969bc3167ceb5fe4431c5ef4c9e5865
Aleksey <3ton1995@mail.ru>
2023-01-18 22:22:41 +0300

    second commit

---
+++ src/main/card.js
@@ -0,0 +1,79 @@
+import "./card.css";
+import React, { useState } from "react";
+
+const Card = (props) => {
+  const { data } = props;
+
+  const [selected, setSelected] = useState("card");
+  const [underText, setUnderText] = useState("Чего сидишь? Порадуй котэ, ");
+  const [byeText, setByeText] = useState("купи.");
+
+  const select = () => {
+    console.log("click");
+    if (selected === "card") {
+      setSelected("card_selected");
+      setUnderText(data.underText);
+      setByeText("");
+    } else {
+      setSelected("card");
+      setUnderText("Чего сидишь? Порадуй котэ, ");
+      setByeText("купи.");
+    }
+    console.log(selected);
+  };
+
+  const bye = {
+    borderStyle: "dashed",
+    borderWidth: "0 0 1px 0",
+    color: "#1698D9",
+    cursor: "pointer",
+  };
+
+  return (
+    <div>
+      <div className="cardBox">
+        <div className={selected} onClick={select}>
+          <div className="yastvo">{data.firstLine}</div>
+          <div className="nyamyshka">{data.name}</div>
+          <div className="nachinka">{data.nachinka}</div>
+          <div className="oval">
+            <div className="weight">{data.weight}</div>
+          </div>
+
+          <div className="kg">кг</div>
+          <div className="portions">
+            <span style={{ fontWeight: "700" }}>
+              {`${data.portions.quantity}`}
+            </span>
+            {` порций`}
+            <br />{" "}
+            <span
+              style={{
+                fontFamily: "Trebuchet MS",
+                fontWeight: "700",
+
+                fontSize: "14px",
+
+                lineHeight: "16px",
+                color: "#666666",
+              }}
+            >
+              {data.gift.quantity}
+            </span>{" "}
+            {data.gift.text}
+            <br />
+            {data.gift.addText}
+          </div>
+        </div>
+        <div className="under">
+          {underText}
+          <span style={bye} onClick={select}>
+            {byeText}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/main/main.js
@@ -0,0 +1,74 @@
+import Card from "./card";
+import "./main.css";
+import React from "react";
+
+const Main = () => {
+  const packetWithFoieGras = {
+    firstLine: "Сказачное заморское яство",
+    name: "Нямушка",
+    nachinka: "с фуа-гра",
+    weight: "0,5",
+    portions: {
+      quantity: "10",
+      text: "порций",
+    },
+    gift: {
+      quantity: "",
+      text: "мышь в подарок",
+    },
+    underText: "Печень утки разварная с артишоками.",
+  };
+
+  const packetWithFish = {
+    firstLine: "Сказачное заморское яство",
+    name: "Нямушка",
+    nachinka: "с рыбой",
+    weight: "2",
+    portions: {
+      quantity: "40",
+      text: "порций",
+    },
+    gift: {
+      quantity: "2",
+      text: "мыши в подарок",
+    },
+    underText: "Головы щучьи с чесноком да свежайшая сёмгушка.",
+  };
+
+  const packetWithChicken = {
+    firstLine: "Сказачное заморское яство",
+    name: "Нямушка",
+    nachinka: "с курой",
+    weight: "5",
+    portions: {
+      quantity: "100",
+      text: "порций",
+    },
+    gift: {
+      quantity: "5",
+      text: "мышей в подарок",
+      addText: "заказчик доволен",
+    },
+    underText: "Филе из цыплят с трюфелями в бульоне.",
+  };
+  return (
+    <div className="mainDiv">
+      <div>
+        <div className="heading">Ты сегодня покормил кота?</div>
+        <div className="conteinerCard">
+          <div className="card1">
+            <Card data={packetWithFoieGras} />
+          </div>
+          <div className="card2">
+            <Card disabled data={packetWithFish} />
+          </div>
+          <div className="card3">
+            <Card data={packetWithChicken} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
